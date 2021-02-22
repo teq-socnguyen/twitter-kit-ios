9ec0b52460bb2d94e9532bb41259f901dbfabe5e
teq-socnguyen <78474109+teq-socnguyen@users.noreply.github.com>
2021-02-22 16:49:57 +0700

    Update TWTRSETweetTextViewContainer.m

--- TwitterKit/TwitterKit/TwitterShareExtensionUI/Private/Composer/TWTRSETweetTextViewContainer.m
+++ TwitterKit/TwitterKit/TwitterShareExtensionUI/Private/Composer/TWTRSETweetTextViewContainer.m
@@ -181,6 +181,13 @@ static const UIEdgeInsets kComposeTextViewTextContainerInsets = {.top = 8, .left
     self.characterCounterLabel.textColor = ([self.tweet isNearOrOverCharacterLimit]) ? _characterCountOverLimitColor : _characterCountBelowLimitColor;
 }
 
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    [self _tseui_updateLineCount];
+}
+
 - (void)_tseui_updateLineCount
 {
     NSUInteger textViewNumberOfLines = _textView.numberOfLines;
